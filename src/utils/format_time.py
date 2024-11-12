5a1f99a93d4434137b4e13380b23723556d8e81c
mwoodhull <mwoodhull5@gmail.com>
2024-11-12 15:06:32 +0000

    removes f string as this is no longer required

--- src/utils/format_time.py
+++ src/utils/format_time.py
@@ -1,7 +1,7 @@
 def format_time(date_time):
 
 
-    date_time_string = date_time.strftime(f"%Y-%m-%d %H:%M:%S.%f")
+    date_time_string = date_time.strftime("%Y-%m-%d %H:%M:%S.%f")
     formatted_time = date_time_string[:-3]
 
     return formatted_time
