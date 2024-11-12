73739b256eb691990bbea14e8c24ef9b8dc668e7
Aidan-Mooney <aidanmooney2001@gmail.com>
2024-11-12 13:54:13 +0000

    complete generate_new_entry_query function plus tests

---
+++ src/utils/generate_new_entry_query.py
@@ -0,0 +1,33 @@
+import re
+
+
+class DateFormatError(Exception):
+    pass
+
+
+def generate_new_entry_query(table_name: str, start_time: str, end_time: str):
+    """
+    Return a valid PostgreSQL query string for rows which were modified in the given table between the start_time and end_time. Will raise DateFormatError if start_time or end_time are in an invalid format. Valid formats include:
+    YYYY-MM-DD
+    YYYY-MM-DD hh:mm:ss
+    YYYY-MM-DD hh:mm:ss.xxx (where xxx is the milliseconds)
+
+    Parameters
+    table_name: str The name of the table in the database
+    start_time: str The start of the time range (inclusive)
+    end_time: str The end of the time range (inclusive)
+    """
+    time_condition = re.compile(r"^\d{4}-\d{2}-\d{2}( \d{2}:\d{2}:\d{2}(.\d{3})?)?$")
+    try:
+        assert time_condition.match(start_time)
+        assert time_condition.match(end_time)
+        query = f"""
+        SELECT *
+        FROM {table_name}
+        WHERE last_updated BETWEEN {start_time} AND {end_time}
+        """
+        return query
+    except AssertionError:
+        raise DateFormatError(
+            "start_time and end_time must be correct format: see docstring"
+        )
---
+++ test/test_generate_new_entry_query.py
@@ -0,0 +1,67 @@
+from src.utils.generate_new_entry_query import generate_new_entry_query, DateFormatError
+from pytest import mark, raises
+
+
+@mark.it("test query inserts table_name, start_time and end_time into query.")
+def test_1():
+    table_name = "test_table_name"
+    start_time = "2024-11-12 12:13:52.962"
+    end_time = "2024-11-13 12:13:52.962"
+    output = """
+        SELECT *
+        FROM test_table_name
+        WHERE last_updated BETWEEN 2024-11-12 12:13:52.962 AND 2024-11-13 12:13:52.962
+        """
+    assert generate_new_entry_query(table_name, start_time, end_time) == output
+
+
+@mark.it("test invalid start time raises DateFormatError")
+def test_2():
+    table_name = "test_table_name"
+    start_time = "I am invalid"
+    end_time = "2024-11-13 12:13:52.962"
+    with raises(DateFormatError) as err:
+        generate_new_entry_query(table_name, start_time, end_time)
+    assert (
+        str(err.value)
+        == "start_time and end_time must be correct format: see docstring"
+    )
+
+
+@mark.it("test invalid end time raises DateFormatError")
+def test_3():
+    table_name = "test_table_name"
+    start_time = "2024-11-13 12:13:52.962"
+    end_time = "I am invalid"
+    with raises(DateFormatError) as err:
+        generate_new_entry_query(table_name, start_time, end_time)
+    assert (
+        str(err.value)
+        == "start_time and end_time must be correct format: see docstring"
+    )
+
+
+@mark.it("test date with no time")
+def test_4():
+    table_name = "test_table_name"
+    start_time = "2024-11-12"
+    end_time = "2024-11-13"
+    output = """
+        SELECT *
+        FROM test_table_name
+        WHERE last_updated BETWEEN 2024-11-12 AND 2024-11-13
+        """
+    assert generate_new_entry_query(table_name, start_time, end_time) == output
+
+
+@mark.it("test date and time with no milliseconds")
+def test_5():
+    table_name = "test_table_name"
+    start_time = "2024-11-12 12:13:52"
+    end_time = "2024-11-13 12:13:52"
+    output = """
+        SELECT *
+        FROM test_table_name
+        WHERE last_updated BETWEEN 2024-11-12 12:13:52 AND 2024-11-13 12:13:52
+        """
+    assert generate_new_entry_query(table_name, start_time, end_time) == output
