361155e96af2d0668a24c18481c8203451a2308a
Matt Clough <mattjamesclough@gmail.com>
2024-11-12 16:55:55 +0000

    fix iam prefixes

--- main-terraform/iam.tf
+++ main-terraform/iam.tf
@@ -73,22 +73,22 @@ resource "aws_iam_policy" "s3_transform_policy" {
 
 #########################################################
 
-resource "iam_role_policy_attachment" "s3_ingest_code_policy" {
+resource "aws_iam_role_policy_attachment" "s3_ingest_code_policy" {
     role = aws_iam_role.ingest_lambda_role.name
     policy_arn = aws_iam_policy.s3_code_policy.arn
 }
 
-resource "iam_role_policy_attachment" "s3_transform_code_policy" {
+resource "aws_iam_role_policy_attachment" "s3_transform_code_policy" {
     role = aws_iam_role.transform_lambda_role.name
     policy_arn = aws_iam_policy.s3_code_policy.arn
 }
 
-resource "iam_role_policy_attachment" "s3_ingest_policy" {
+resource "aws_iam_role_policy_attachment" "s3_ingest_policy" {
     role = aws_iam_role.ingest_lambda_role.name
     policy_arn = aws_iam_policy.s3_ingest_policy.arn
 }
 
-resource "iam_role_policy_attachment" "s3_transform_policy" {
+resource "aws_iam_role_policy_attachment" "s3_transform_policy" {
     role = aws_iam_role.transform_lambda_role.name
     policy_arn = aws_iam_policy.s3_transform_policy.arn
 }
