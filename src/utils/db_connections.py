2669cb7831b3dc37f8f1862257d68819445aa19e
Anthony Tran <anthonytran@MacBookAir.broadband>
2024-11-12 11:20:11 +0000

    gets creds as dictionary from secretsmanager and tested

---
+++ src/utils/db_connections.py
@@ -0,0 +1,18 @@
+import boto3
+from botocore.exceptions import ClientError
+import ast
+
+
+def db_connections_get_secret(client, secret_name):
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = ast.literal_eval(get_secret_value_response['SecretString'])[0]
+    return secret
---
+++ test/test_db_connections.py
@@ -0,0 +1,47 @@
+from moto import mock_aws
+import boto3
+import pytest
+import os
+from src.utils.db_connections import db_connections_get_secret
+import json
+
+class TestDBConnectionGetSecrets:
+    @pytest.fixture(scope="function", autouse=True)
+    def aws_credentials(self):
+        """Mocked AWS Credentials for moto."""
+        os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+        os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+        os.environ["AWS_SECURITY_TOKEN"] = "testing"
+        os.environ["AWS_SESSION_TOKEN"] = "testing"
+        os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+    @pytest.fixture(scope='function')
+    def secretsmanager(self, aws_credentials):
+        with mock_aws():
+            yield boto3.client('secretsmanager', region_name='eu-west-2')
+
+    @pytest.fixture(scope='function',autouse=True)
+    def adding_secret(self, secretsmanager):
+        secret_name = 'secret_test'
+        secret_stuff = {
+            'user': 'project_team',
+            'password': 'secret_password',
+            'host': 'example.com',
+            'database': 'test_db',
+            'port' : 1234
+        },
+        secretsmanager.create_secret(
+            Name=secret_name,
+            SecretString=str(secret_stuff),
+            ForceOverwriteReplicaSecret=True)
+
+    def test_db_connection_read_credentials(self, secretsmanager):   
+        result = db_connections_get_secret(secretsmanager, 'secret_test')
+        assert result == {'user': 'project_team', 
+                          'password': 'secret_password', 
+                          'host': 'example.com', 
+                          'database': 'test_db', 
+                          'port': 1234}
+
+    def test_
+    
