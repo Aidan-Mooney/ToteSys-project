8d7e70157faccbf8325f9b737b639df61eacc89c
mwoodhull <mwoodhull5@gmail.com>
2024-11-12 15:05:43 +0000

    removes psuedocode

--- src/utils/format_time.py
+++ src/utils/format_time.py
@@ -5,7 +5,3 @@ def format_time(date_time):
     formatted_time = date_time_string[:-3]
 
     return formatted_time
-
-    # millisecond = str(math.floor(int(date_time.strftime('%f'))))[:3]
-    # formatted_time = date_time.strftime(f"%Y-%m-%d %H:%M:%S.{millisecond}")
-    # return formatted_time
