2f4f5e354ac94c6fc3e3832c3bc41a5bb5ef3101
mwoodhull <mwoodhull5@gmail.com>
2024-11-13 11:40:37 +0000

    completes testing for parquet data function

--- src/utils/parquet_data.py
+++ src/utils/parquet_data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from io import BytesIO
-# import pyarrow
 
 def parquet_data(py_dict):
     
@@ -9,6 +8,5 @@ def parquet_data(py_dict):
     out_buffer = BytesIO()
     df.to_parquet(out_buffer, index=False)
 
-    return out_buffer
+    return out_buffer.getvalue()
 
-    # parq_read = pd.read_parquet('example.parquet', engine='pyarrow')
