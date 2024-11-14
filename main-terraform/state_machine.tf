f0d75948f2cc44a4ea00f1d1345d81466740017d
Anthony Tran <anthonytran@MacBookAir.broadband>
2024-11-14 18:22:53 +0000

    updates json file to lambda ingest

--- main-terraform/state_machine.tf
+++ main-terraform/state_machine.tf
@@ -3,11 +3,9 @@ resource "aws_sfn_state_machine" "state_machine" {
   name_prefix = var.state_machine_name
   role_arn = aws_iam_role.state_role.arn
   definition = templatefile("${path.module}/../state-machine/state_machine.asl.json", {
-    LambdaIngest = aws_lambda_function.ingest_lambda_function.arn
-    # LambdaTransform = aws_lambda_function.transform_lambda_function.arn
+    lambda_ingest = aws_lambda_function.ingest_lambda_function.arn
   }
   )
- 
 }
 
 data "aws_caller_identity" "current" {}
