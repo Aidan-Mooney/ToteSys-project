71cd4dfde89d25310173d5bd6f2a39e06ada6878
Aidan-Mooney <aidanmooney2001@gmail.com>
2024-11-14 15:34:57 +0000

    change .name to .id

--- s3-terraform/ssm.tf
+++ s3-terraform/ssm.tf
@@ -15,14 +15,14 @@ resource "aws_ssm_parameter" "transform_bucket_arn" {
 resource "aws_ssm_parameter" "ingest_bucket_name" {
   name  = "ingest_bucket_name"
   type  = "String"
-  value = "${aws_s3_bucket.ingest_bucket2_name}"
+  value = "${aws_s3_bucket.ingest_bucket2.id}"
 #   overwrite = true
 }
 
 resource "aws_ssm_parameter" "transform_bucket_name" {
   name  = "transform_bucket_name"
   type  = "String"
-  value = "${aws_s3_bucket.transform_bucket_name}"
+  value = "${aws_s3_bucket.transform_bucket.id}"
 #   overwrite = true
 }
 
