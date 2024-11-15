ee21541c27d89a2774ee17b6c61cde79f609dc62
Matt Clough <mattjamesclough@gmail.com>
2024-11-15 09:49:08 +0000

    change dir structure

---
+++ immutable-terraform/ssm.tf
@@ -0,0 +1,28 @@
+resource "aws_ssm_parameter" "ingest_bucket_arn" {
+  name  = "ingest_bucket_arn"
+  type  = "String"
+  value = "${aws_s3_bucket.ingest_bucket2.arn}"
+#   overwrite = true
+}
+
+resource "aws_ssm_parameter" "transform_bucket_arn" {
+  name  = "transform_bucket_arn"
+  type  = "String"
+  value = "${aws_s3_bucket.transform_bucket.arn}"
+#   overwrite = true
+}
+
+resource "aws_ssm_parameter" "ingest_bucket_name" {
+  name  = "ingest_bucket_name"
+  type  = "String"
+  value = "${aws_s3_bucket.ingest_bucket2.id}"
+#   overwrite = true
+}
+
+resource "aws_ssm_parameter" "transform_bucket_name" {
+  name  = "transform_bucket_name"
+  type  = "String"
+  value = "${aws_s3_bucket.transform_bucket.id}"
+#   overwrite = true
+}
+
---
+++ immutable-terraform/vars.tf
@@ -0,0 +1,9 @@
+variable "ingest_bucket_prefix" {
+  type    = string
+  default = "ingest2"
+}
+
+variable "transform_bucket_prefix" {
+  type    = string
+  default = "transform"
+}
