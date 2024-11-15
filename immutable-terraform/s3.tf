0a852fe70c8094ed87e943766c8e6a6c0359ab8e
Matt Clough <mattjamesclough@gmail.com>
2024-11-15 09:50:05 +0000

    disable object lock for debugging purposes

--- immutable-terraform/s3.tf
+++ immutable-terraform/s3.tf
@@ -6,22 +6,22 @@ resource "aws_s3_bucket" "ingest_bucket2" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "ingest_bucket_version" {
-  bucket = aws_s3_bucket.ingest_bucket2.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "ingest_bucket_version" {
+#   bucket = aws_s3_bucket.ingest_bucket2.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_object_lock_configuration" "ingest_bucket_config" {
-  bucket = aws_s3_bucket.ingest_bucket2.id
-  rule {
-    default_retention {
-      mode = "GOVERNANCE"
-      days = 1
-    }
-  }
-}
+# resource "aws_s3_bucket_object_lock_configuration" "ingest_bucket_config" {
+#   bucket = aws_s3_bucket.ingest_bucket2.id
+#   rule {
+#     default_retention {
+#       mode = "GOVERNANCE"
+#       days = 1
+#     }
+#   }
+# }
 
 resource "aws_s3_bucket" "transform_bucket" {
   bucket_prefix = var.transform_bucket_prefix
@@ -31,19 +31,19 @@ resource "aws_s3_bucket" "transform_bucket" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "transform_bucket_version" {
-  bucket = aws_s3_bucket.transform_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "transform_bucket_version" {
+#   bucket = aws_s3_bucket.transform_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_object_lock_configuration" "transform_bucket_config" {
-  bucket = aws_s3_bucket.transform_bucket.id
-  rule {
-    default_retention {
-      mode = "GOVERNANCE"
-      days = 1
-    }
-  }
-}
+# resource "aws_s3_bucket_object_lock_configuration" "transform_bucket_config" {
+#   bucket = aws_s3_bucket.transform_bucket.id
+#   rule {
+#     default_retention {
+#       mode = "GOVERNANCE"
+#       days = 1
+#     }
+#   }
+# }
