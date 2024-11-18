bcf0521de5ceea3547e4a4cfb95788a8a843e7c1
Matt Clough <mattjamesclough@gmail.com>
2024-11-18 11:29:15 +0000

    💋

--- df_scoping/df_scoping.py
+++ df_scoping/df_scoping.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from os import listdir
+from pandas import DataFrame
+# do we need to worry about foreign keys in the warehouse? I think not, as we won't be deleting anything.
+# use df.dt.time to get the time
+# use df.dt.date to get the date
 
 
 def make_dfs(dir, extension=".parquet"):
@@ -10,6 +14,27 @@ def make_dfs(dir, extension=".parquet"):
     return dataframes
 
 
+def fact_sales_order(sales_order: DataFrame):
+    existing_cols = sales_order[
+        "sales_order_id",
+        "design_id",
+        "staff_id",
+        "counterparty_id",
+        "units_sold",
+        "unit_price",
+        "currency_id",
+        "agreed_delivery_date",
+        "agreed_payment_date",
+        "agreed_delivery_location_id",
+    ]
+    # rename staff_id
+    # add columns for date and time for last_updated and created_at
+    return sales_order
+
+
 if __name__ == "__main__":
     dataframes = make_dfs("df_scoping/tables/")
-    print(dataframes)
+    # print(dataframes["sales_order"])
+    # print(dataframes["design"])
+    print(fact_sales_order(dataframes["sales_order"]))
+    print(fact_sales_order(dataframes["sales_order"]).dt.date)
