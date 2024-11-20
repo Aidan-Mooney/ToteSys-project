6cb214462bd9843da1069de2df0c6fd963c948e5
Matt Clough <mattjamesclough@gmail.com>
2024-11-20 09:05:41 +0000

    logger

--- src/transform_utils/get_parquet_body_from_s3.py
+++ src/transform_utils/get_parquet_body_from_s3.py
@@ -1,12 +1,14 @@
 # in: filename, bucket_name
 # out: parquet data
 from boto3 import client
+from logging import getLogger
 
+logger = getLogger(__name__)
 s3_client = client("s3")
 
 
 def get_parquet_body_from_s3(filename, bucket_name):
-    print(s3_client.list_buckets())
+    logger.critical(s3_client.list_buckets())
     body = (
         s3_client.get_object(Bucket=bucket_name, Key=filename)["Body"]
         .read()
