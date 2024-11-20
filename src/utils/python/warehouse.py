3c282d9afa0be10658fa607751da68d6addf0d63
Matt Clough <mattjamesclough@gmail.com>
2024-11-20 14:32:04 +0000

    add s3 client to warehouse init

--- src/utils/python/warehouse.py
+++ src/utils/python/warehouse.py
@@ -9,7 +9,7 @@ else:
 
 
 class Warehouse:
-    def __init__(self, list_of_filenames: list[str], bucket_name: str):
+    def __init__(self, list_of_filenames: list[str], bucket_name: str, s3_client):
         """
         Warehouse object expects parquet file from ingest bucket and creates dim- and fact- tables.
 
@@ -31,7 +31,7 @@ class Warehouse:
             fact_payment (depends on payment)
             fact_purchase_order (depends on purchase_order)
         """
-        self.s3_client = client("s3")
+        self.s3_client = s3_client
         self.dataframes = {}
         for filename in list_of_filenames:
             table_name = filename[: filename.index("/")]
