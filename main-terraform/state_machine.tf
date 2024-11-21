2e472c2aed620bbfd8c7e2ebff552650ac379beb
Aidan-Mooney <aidanmooney2001@gmail.com>
2024-11-21 10:04:22 +0000

    separates payload from statemachine

--- main-terraform/state_machine.tf
+++ main-terraform/state_machine.tf
@@ -3,6 +3,7 @@ resource "aws_sfn_state_machine" "state_machine" {
   name_prefix = var.state_machine_name
   role_arn = aws_iam_role.state_role.arn
   definition = templatefile("${path.module}/../state-machine/state_machine.asl.json", {
+    payload = file("${path.module}/../state-machine/init_payload.json")
     lambda_ingest = aws_lambda_function.ingest_lambda_function.arn
     lambda_transform = aws_lambda_function.transform_lambda_function.arn
   }
