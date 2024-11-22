d7e0c56a66046eb101f62475d56c878b6d7e0115
Anthony Tran <anthonytran@MacBookAir.broadband>
2024-11-22 16:06:17 +0000

    completes doc string to the function

--- src/utils/python/create_fact_query.py
+++ src/utils/python/create_fact_query.py
@@ -5,18 +5,30 @@ from pg8000.native import literal
 
 def create_fact_query(table_name: str, table_path: str, s3_client) -> str:
     """
-    SQL Query to check if it exists, return True/False
-    If True we append to the existing fact table
-    if False we need to create the fact table and insert values into table
-    return query string to be used to update the table (in a separate function)
+    This creates a query for fact tables
+
+    Input:
+        - table_name: gives the name of the fact table
+        - table_path: gives the file_key for the fact table
+        - s3_client: client for s3
+
+    Process:
+        - it gets the panda dataframe from corresponding fact_table and file_key
+        - fact_cols gets the column names of the fact table
+        - adds the fact_cols to the beginning of the insert query statement
+        - fact_vals gets a list of values/rows of the fact table
+        - finally appends values/rows to the query in the correct format
+    
+    Output:
+        - return a query string to update fact table
     """
 
     df = get_df_from_s3_parquet(
         s3_client, bucket_name=environ["transform_bucket_name"], filename=table_path
     )
 
-    facts_cols = df.columns.values.tolist()
-    cols_string = ", ".join(facts_cols)
+    fact_cols = df.columns.values.tolist()
+    cols_string = ", ".join(fact_cols)
 
     query = f"INSERT INTO {table_name} ({cols_string}) VALUES "
 
