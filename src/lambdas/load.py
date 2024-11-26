895b541138bae957b01ed1bc7d39e0318bf592cc
Matt Clough <mattjamesclough@gmail.com>
2024-11-26 09:06:03 +0000

    remove create fatc table step

--- src/lambdas/load.py
+++ src/lambdas/load.py
@@ -4,7 +4,7 @@ from os import environ
 from boto3 import client
 from pg8000.core import DatabaseError
 
-if environ['DEV_ENVIRONMENT'] == 'testing':
+if environ["DEV_ENVIRONMENT"] == "testing":
     from src.utils.python.create_fact_tables import create_fact_tables
     from src.utils.python.db_connections import close_db_connection, connect_to_db
     from src.utils.python.generate_warehouse_query import generate_warehouse_query
@@ -16,16 +16,28 @@ else:
     from query_db import query_db
 
 logger = getLogger(__name__)
+
+
 def lambda_handler(event, context):
-    s3_client = client('s3')
-    try:
-        create_fact_tables(connect_to_db, close_db_connection)
-    except DatabaseError as de:
-        logger.critical(f'Failed to create fact tables: {de}')
+    s3_client = client("s3")
+    # try:
+    #     create_fact_tables(connect_to_db, close_db_connection)
+    # except DatabaseError as de:
+    #     logger.critical(f'Failed to create fact tables: {de}')
     for table_name in event:
         query = generate_warehouse_query(table_name, event[table_name], s3_client)
         try:
-            query_db(query, connect_to_db, close_db_connection, table_name, 'totesys_warehouse_credentials')
-            logger.info(f'Successfully updated warehouse table {table_name} from path {event[table_name]}')
+            query_db(
+                query,
+                connect_to_db,
+                close_db_connection,
+                table_name,
+                "totesys_warehouse_credentials",
+            )
+            logger.info(
+                f"Successfully updated warehouse table {table_name} from path {event[table_name]}"
+            )
         except DatabaseError as de:
-            logger.critical(f'Failed database query for {table_name} at path {event[table_name]}: {de}')
+            logger.critical(
+                f"Failed database query for {table_name} at path {event[table_name]}: {de}"
+            )
