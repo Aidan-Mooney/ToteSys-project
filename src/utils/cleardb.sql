4a679e6ed8cfb9d06628b2d7ea5e92f9b88b4ae5
Chris Bannister(devPanda) <mail@devpanda.uk>
2024-11-26 14:09:41 +0000

    with more tables

--- src/utils/cleardb.sql
+++ src/utils/cleardb.sql
@@ -1,7 +1,15 @@
 START TRANSACTION;
 
-DELETE * FROM dim_counterparty;
-DELETE * FROM dim_currency;
-DELETE * FROM dim_data;
+DELETE FROM fact_payment;
+DELETE FROM fact_purchase_order;
+DELETE FROM fact_sales_order;
+
+DELETE FROM dim_counterparty;
+DELETE FROM dim_currency;
+DELETE FROM dim_design;
+DELETE FROM dim_location;
+DELETE FROM dim_payment_type;
+DELETE FROM dim_staff;
+DELETE FROM dim_transaction;
 
 COMMIT;
