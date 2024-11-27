c579848906bbeffbd0188b1ff78ce3d7ebdecd5d
Aidan-Mooney <aidanmooney2001@gmail.com>
2024-11-27 11:47:08 +0000

    reformats lambda file a little

--- main-terraform/lambda.tf
+++ main-terraform/lambda.tf
@@ -31,27 +31,28 @@ resource "aws_lambda_function" "ingest_lambda_function" {
   s3_key                = aws_s3_object.ingest_lambda_file.id
   runtime               = var.python_runtime
   depends_on            = [ 
-                            # aws_s3_object.ingest_lambda_file,
-                            # aws_s3_object.dependencies_lambda_file,   <- See pull request message
-                            # aws_s3_object.utils_file,
-                            # aws_iam_role_policy_attachment.ingest_policy,
-                            aws_iam_role_policy_attachment.lambda_logs_for_ingest_policy,
-                            aws_cloudwatch_log_group.totesys-cw-log-group,
-                          ]
+    # aws_s3_object.ingest_lambda_file,
+    # aws_s3_object.dependencies_lambda_file,   <- See pull request message
+    # aws_s3_object.utils_file,
+    # aws_iam_role_policy_attachment.ingest_policy,
+    aws_iam_role_policy_attachment.lambda_logs_for_ingest_policy,
+    aws_cloudwatch_log_group.totesys-cw-log-group,
+  ]
   timeout               = var.default_timeout
   handler               = "${var.ingest_lambda_name}.lambda_handler"
   layers                = [
-                            "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:14",
-                            aws_lambda_layer_version.utils.arn,
-                            aws_lambda_layer_version.dependencies.arn
-                          ]
+    "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:14",
+    aws_lambda_layer_version.utils.arn,
+    aws_lambda_layer_version.dependencies.arn
+  ]
   description           = "Lambda function for extracting data from the database and saving it to the bucket '${data.aws_ssm_parameter.ingest_bucket_name.value}'."
   environment {
-    variables = { ingest_bucket_name = data.aws_ssm_parameter.ingest_bucket_name.value,
-                  DEV_ENVIRONMENT = "deploy",
-                  static_address_path = var.static_address_path,
-                  static_department_path=var.static_department_path
-                }
+    variables = { 
+      ingest_bucket_name = data.aws_ssm_parameter.ingest_bucket_name.value,
+      DEV_ENVIRONMENT = "deploy",
+      static_address_path = var.static_address_path,
+      static_department_path=var.static_department_path
+    }
   }
   logging_config {
     log_format  = "Text"
@@ -67,27 +68,28 @@ resource "aws_lambda_function" "transform_lambda_function" {
   s3_key                = aws_s3_object.transform_lambda_file.id
   runtime               = var.python_runtime
   depends_on            = [ 
-                            # aws_s3_object.transform_lambda_file,
-                            # aws_iam_role_policy_attachment.transform_policy,   <- See pull request message
-                            # aws_s3_object.utils_file,
-                            aws_iam_role_policy_attachment.lambda_logs_for_ingest_policy,
-                            aws_cloudwatch_log_group.totesys-cw-log-group
-                          ]
+    # aws_s3_object.transform_lambda_file,
+    # aws_iam_role_policy_attachment.transform_policy,   <- See pull request message
+    # aws_s3_object.utils_file,
+    aws_iam_role_policy_attachment.lambda_logs_for_ingest_policy,
+    aws_cloudwatch_log_group.totesys-cw-log-group
+  ]
   timeout               = var.default_timeout
   handler               = "${var.transform_lambda_name}.lambda_handler"
   layers                = [
-                            "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:14",
-                            aws_lambda_layer_version.utils.arn
-                          ] 
+    "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:14",
+    aws_lambda_layer_version.utils.arn
+  ] 
   description           = "Lambda function for transforming data stored in the bucket '${data.aws_ssm_parameter.ingest_bucket_name.value}' saving it to the bucket '${data.aws_ssm_parameter.transform_bucket_name.value}'."
   environment {
-    variables = {ingest_bucket_name = data.aws_ssm_parameter.ingest_bucket_name.value, 
-                 transform_bucket_name = data.aws_ssm_parameter.transform_bucket_name.value, 
-                 DEV_ENVIRONMENT = "deploy"
-                 static_address_path = var.static_address_path,
-                 static_department_path=var.static_department_path
-                }
-              }
+    variables = {
+      ingest_bucket_name = data.aws_ssm_parameter.ingest_bucket_name.value, 
+      transform_bucket_name = data.aws_ssm_parameter.transform_bucket_name.value, 
+      DEV_ENVIRONMENT = "deploy"
+      static_address_path = var.static_address_path,
+      static_department_path=var.static_department_path
+    }
+  }
   logging_config {
     log_format  = "Text"
     log_group   = aws_cloudwatch_log_group.totesys-cw-log-group.name
@@ -112,17 +114,17 @@ resource "aws_lambda_function" "load_lambda_function" {
   timeout               = var.default_timeout
   handler               = "${var.load_lambda_name}.lambda_handler"
   layers                = [
-                            "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:14",
-                            aws_lambda_layer_version.utils.arn,
-                            aws_lambda_layer_version.dependencies.arn
-                          ] 
+    "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python312:14",
+    aws_lambda_layer_version.utils.arn,
+    aws_lambda_layer_version.dependencies.arn
+  ] 
   description           = "Lambda function for loading data stored in the bucket '${data.aws_ssm_parameter.transform_bucket_name.value}' to the data warehouse."
   environment {
     variables = { 
-                 transform_bucket_name = data.aws_ssm_parameter.transform_bucket_name.value, 
-                 DEV_ENVIRONMENT = "deploy"
-                }
-              }
+      transform_bucket_name = data.aws_ssm_parameter.transform_bucket_name.value, 
+      DEV_ENVIRONMENT = "deploy"
+    }
+  }
   logging_config {
     log_format  = "Text"
     log_group   = aws_cloudwatch_log_group.totesys-cw-log-group.name
