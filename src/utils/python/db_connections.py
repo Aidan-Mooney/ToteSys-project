42c31bf8f199674501d48e7511cf55dbf757e642
Matt Clough <mattjamesclough@gmail.com>
2024-11-27 11:05:19 +0000

    add docstrings

--- src/utils/python/db_connections.py
+++ src/utils/python/db_connections.py
@@ -4,9 +4,25 @@ import ast
 import pg8000.native
 
 
-def db_connections_get_secret(client, secret_name):
+def db_connections_get_secret(sm_client, secret_name: str):
+    """
+    Retrive the secret stored in AWS SecretsManager with name secret_name.
+
+    Parameters:
+    sm_client: AWS SecretsManager client
+    secret_name: name of string stored in SecretsManager.
+
+    Structure of the secret as a json object:
+        {
+            "user": "...",
+            "password": "...",
+            "host": "...",
+            "database": "...",
+            "port": ...
+        }
+    """
     try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        get_secret_value_response = sm_client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
         # For a list of exceptions thrown, see
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
@@ -17,6 +33,11 @@ def db_connections_get_secret(client, secret_name):
 
 
 def connect_to_db(secret_name: str = "totesys_db_credentials"):
+    """
+    Return a pg8000 connection object, connected to the database corresponding to the credentials given.
+    Parameters:
+    secret_name: name of the secret in SecretsManager which contains the DB credentials. Defaults to "totesys_db_credentials". May only be "totesys_db_credentials" or "totesys_warehouse_credentials"
+    """
     assert secret_name in ["totesys_db_credentials", "totesys_warehouse_credentials"]
     secretsmanager = boto3.client("secretsmanager")
     creds = db_connections_get_secret(secretsmanager, secret_name)
