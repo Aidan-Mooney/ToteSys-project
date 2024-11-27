364c529adeb2ee9aeb331c15ab7147610fc33d3d
Matt Clough <mattjamesclough@gmail.com>
2024-11-27 11:09:25 +0000

    add microseconds to docstring

--- src/utils/python/generate_new_entry_query.py
+++ src/utils/python/generate_new_entry_query.py
@@ -8,16 +8,19 @@ class DateFormatError(Exception):
 def generate_new_entry_query(table_name: str, start_time: str, end_time: str):
     """
     Return a valid PostgreSQL query string for rows which were modified in the given table between the start_time and end_time. Will raise DateFormatError if start_time or end_time are in an invalid format. Valid formats include:
-    YYYY-MM-DD
-    YYYY-MM-DD hh:mm:ss
-    YYYY-MM-DD hh:mm:ss.xxx (where xxx is the milliseconds)
+    - YYYY-MM-DD
+    - YYYY-MM-DD hh:mm:ss
+    - YYYY-MM-DD hh:mm:ss.xxx (where xxx are milliseconds)
+    - YYYY-MM-DD hh:mm:ss.ususus (where ususus is microseconds)
 
     Parameters
     table_name: str The name of the table in the database
     start_time: str The start of the time range (inclusive)
     end_time: str The end of the time range (exclusive)
     """
-    time_condition = re.compile(r"^\d{4}-\d{2}-\d{2}( \d{2}:\d{2}:\d{2}(.\d{3})?(\d{3})?)?$")
+    time_condition = re.compile(
+        r"^\d{4}-\d{2}-\d{2}( \d{2}:\d{2}:\d{2}(.\d{3})?(\d{3})?)?$"
+    )
     try:
         assert time_condition.match(start_time)
         assert time_condition.match(end_time)
