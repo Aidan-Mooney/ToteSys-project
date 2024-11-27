fcb3d8654bb80e7ac27775e9a5d2d24ef12e8530
Matt Clough <mattjamesclough@gmail.com>
2024-11-27 11:07:59 +0000

    add docstring

--- src/utils/python/format_time.py
+++ src/utils/python/format_time.py
@@ -1,6 +1,12 @@
-def format_time(date_time):
+from datetime import datetime
 
 
+def format_time(date_time: datetime):
+    """
+    Return a string-formatted representation of the datetime object date_time of the format YY-MM-DD hh:mm:ss.ususus
+
+    Parameters:
+    - date_time: datetime object
+    """
     date_time_string = date_time.strftime("%Y-%m-%d %H:%M:%S.%f")
-
     return date_time_string
